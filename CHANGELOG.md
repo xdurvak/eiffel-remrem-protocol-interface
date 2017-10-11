28760ec820948420a582ed2412de6c9d3a631e69
xumakap <kapu.umadevi@tcs.com>
2017-10-11 11:34:39 +0530

    Added getRoutingKey method in MsgService interface (#3)
    * Added getRoutingKey method in MsgService interface
    
    * Added domainId method in MsgService interface
    
    * Changed domainId to domain and added javadoc.
    
    * Add javadoc for param eiffelMessage
    
    * changed log message in CHANGELOG.md file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.3
+- Added method to get Routing key.
+
 ## 0.0.2
 - Added copyright headers to the source code.
 
--- src/main/java/com/ericsson/eiffel/remrem/protocol/MsgService.java
+++ src/main/java/com/ericsson/eiffel/remrem/protocol/MsgService.java
@@ -65,4 +65,27 @@ public interface MsgService {
      * @return ValidationResult with true if validation is success, if validation fails ValidationResult has false and validation message property's.
      */
     ValidationResult validateMsg(String msgType, JsonObject jsonvalidateMessage);
+
+    /**
+     * Returns the domain Id from json formatted eiffel message.
+     * @param eiffelMessage eiffel message in json format
+     * @return the domainId from eiffelMessage if domainId not available then returns the null value
+     */
+    String getDomainId(JsonObject eiffelMessage);
+
+    /**
+     * Returns Routing key from the messaging library based on the eiffel message eventType.<br>
+     * In general, Routing key of eiffel message is in the format<br>
+                 <b>&lt;protocol&gt;.&lt;family&gt;.&lt;type&gt;.&lt;tag&gt;.&lt;domain&gt;</b><br>
+     * &lt;protocol&gt; is used if provided by the protocol library used.<br>
+     * &lt;family&gt; and &lt;type&gt; are provided by the protocol library.<br>
+     * &lt;tag&gt; which needs to be put in the Routing key<br>
+     * &lt;domain&gt; is configured and can be suffixed by a user domain.<br>
+     * @param eiffelMessage eiffel message in json format
+     * @param tag
+     * @param domain from which the message is sent
+     * @param userDomainSuffix
+     * @return Routing key in String format.
+     */
+    String getRoutingKey(JsonObject eiffelMessage, String tag, String domain, String userDomainSuffix);
 }
